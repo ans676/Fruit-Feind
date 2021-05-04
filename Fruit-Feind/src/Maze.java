68dfacb5dbde880674dcc5d3be94b22b421775f6
ans676 <ans676@nyu.edu>
2021-05-04 15:56:46 -0400

    Working on the Walls class Can not get the user to not walk throughwalls

--- Fruit-Feind/src/Maze.java
+++ Fruit-Feind/src/Maze.java
@@ -10,7 +10,7 @@ public class Maze extends PApplet{
 	
 	User player; 
 
-	Walls [] wall = {new Walls(this,50, 50, 50, 450), new Walls(this,50, 450, 300, 450),
+	Walls wall[] = {new Walls(this,50, 50, 50, 450), new Walls(this,50, 450, 300, 450),
 			new Walls(this,300, 450, 300, 550), new Walls(this,300, 550, 50, 550), new Walls(this,150, 50, 250, 50), 
 			new Walls(this, 250, 50, 250, 150), new Walls(this,250, 150, 550, 150), new Walls(this,550, 150, 550, 450), 
 			new Walls(this, 450, 550, 450, 250), new Walls(this,450, 250, 150, 250)};
@@ -33,29 +33,91 @@ public class Maze extends PApplet{
 		
 		
 	}
-	public boolean collision() {
-		if (player.xpos == )
-	}
 	
 	public void keyPressed() {
 		if(keyCode == UP) {
-			player.moveY(-10);
+			if (checkCollision(player.xpos, player.ypos, wall, -2)) {
+				System.out.print("Collision");
+			}
+			else {
+				player.moveY(-2);
+			}
 			
 		}
 		else if (keyCode == DOWN) {
-			player.moveY(10);
+			if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+				System.out.print("Collision");
+			}
+			else {
+				player.moveY(2);
+			}
+			
+			//if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+			//	player.moveY(2);
+			//}	
 		}
 		else if (keyCode == LEFT) {
-			player.moveX(-10);
+			if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+				System.out.print("Collision");
+			}
+			else {
+				player.moveX(-2);
+			}
+			//if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+			//	player.moveX(-2);
+			//}
+			//player.moveX(-2);
 		}
 		else if (keyCode == RIGHT) {
-			player.moveX(10);
+			if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+				System.out.print("Collision");
+			}
+			else {
+				player.moveX(2);
+			}
+			
+			
+			//if (checkCollision(player.xpos, player.ypos, wall, 2)) {
+			//	player.moveX(2);
+			//}
+			//player.moveX(2);
 		}
-		
+		 
 	}
 	public void keyReleased( ) {
 		player.moveX(0);
 		player.moveY(0);
+	}
+	
+	public boolean checkCollision(float playerX, float playerY, Walls wallList[], int step) {
+		for (int i = 0; i < wallList.length; i++) {
+			
+			int wallX1 = wallList[i].x;
+			int wallY1 = wallList[i].y;
+			int wallX2 = wallList[i].x2;
+			int wallY2 = wallList[i].y2;
+			
+			
+			if (wallX1 == wallX2 ) {
+				System.out.print("x's equal ");
+				System.out.println("Wall" + i + " " + wallX1 + " " + wallX2 + " " + wallY1 + " " + wallY2);
+
+				if (playerX == wallX1 + step) {
+					if ( playerY + step > wallY1 || playerY + step > wallY2) {
+				return true; 
+				}}
+			}
+			else if (wallList[i].y == wallList[i].y2 ) {
+				System.out.print("y's equal ");
+				System.out.println("Wall" + i + " " + wallX1 + " " + wallX2 + " " + wallY1 + " " + wallY2);
+
+				if (playerY == wallY1 + step) {
+					if ( playerX + step > wallX1 || playerX + step < wallY2) {
+						return true; 
+				}}}
+		}
+			
+		return false;
 	}
 	
 	
--- Fruit-Feind/src/Walls.java
+++ Fruit-Feind/src/Walls.java
@@ -5,15 +5,15 @@ public class Walls extends PApplet{
 	PApplet canvas;
 	int x;
 	int y;
-	int width;
-	int height;
+	int x2;
+	int y2;
 
-	public Walls (PApplet canvas, int x, int y, int width, int height) {
+	public Walls (PApplet canvas, int x, int y, int x2, int y2) {
 		this.canvas = canvas;	
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.x2 = x2;
+		this.y2 = y2;
 	}
 	
 	public void update() {
@@ -21,7 +21,7 @@ public class Walls extends PApplet{
 	}
 
 	public void show() {
-		this.canvas.line(x, y, width, height);
+		this.canvas.line(x, y, x2, y2);
 	}}
 	
 	      
